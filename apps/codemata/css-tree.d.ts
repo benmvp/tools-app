8f3ecfa1224c0d2c5fe6e6f5445a20e114bfa5f7
Ben Ilegbodu <ben@benmvp.com>
2026-01-25 16:20:51 -0600

    Fix type check

---
+++ apps/codemata/css-tree.d.ts
@@ -0,0 +1,18 @@
+declare module "css-tree" {
+  export interface ParseError {
+    message: string;
+    line: number;
+    column: number;
+    offset?: number;
+    name?: string;
+    formattedMessage?: string;
+  }
+
+  export interface ParseOptions {
+    context?: string;
+    positions?: boolean;
+    onParseError?: (error: ParseError) => void;
+  }
+
+  export function parse(input: string, options?: ParseOptions): any;
+}
